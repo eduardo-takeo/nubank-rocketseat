b00259403ddeb0a0ecf4e4f68f4dffdbc28d2de3
eduardo-takeo <dudu.takeo96@gmail.com>
2020-04-12 19:06:33 -0300

    initial commit

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Container} from './styles';
+
+const Main = () => {
+    return (
+        <Container>
+            <Text>Nubank</Text>
+        </Container>
+    )
+}
+
+export default Main
